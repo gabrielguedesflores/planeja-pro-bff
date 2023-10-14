fefacd10b46f0489b7e4e91f7589d6de12ae5a12
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2023-10-14 17:11:35 -0300

    feat: rota /

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HomeModule } from 'src/application/controller/home/home.module';
 import { ExpenseModule } from 'src/application/controller/expense/expense.module';
 import { UserModule } from 'src/application/controller/user/user.module';
 
@@ -8,6 +9,7 @@ import { UserModule } from 'src/application/controller/user/user.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.URI_MONGODB),
+    HomeModule,
     ExpenseModule,
     UserModule
   ],
