505b7d7df13e6d7bdfa54106c3f43d6fe55a51f7
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2023-10-14 18:23:55 -0300

    build: configs vercel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { HomeModule } from 'src/application/controller/home/home.module';
-import { ExpenseModule } from 'src/application/controller/expense/expense.module';
-import { UserModule } from 'src/application/controller/user/user.module';
+import { HomeModule } from '../application/controller/home/home.module';
+import { ExpenseModule } from '../application/controller/expense/expense.module';
+import { UserModule } from '../application/controller/user/user.module';
 
 @Module({
   imports: [
