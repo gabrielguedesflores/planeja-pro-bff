4d8416e76174c987fddb300b32bb179563ed14c4
Gabriel Guedes Flores <60441476+gabrielguedesflores@users.noreply.github.com>
2023-10-14 16:26:53 -0300

    fix: change port

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
   app.enableCors();
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
